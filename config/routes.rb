91c86fe404f9de2e54ab395d8e4d490d10595862
david30-maker <davidokpah396@gmail.com>
2024-10-08 18:45:03 +0100

    modify show lesson page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations]
   devise_for :users
   resources :courses do
-    resources :lessons
+    resources :lessons, only: [:show , :update]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
