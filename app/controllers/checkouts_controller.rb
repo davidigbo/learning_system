85bc2d3af7e3e3a2fbf0083a908cee1d9129e53b
david30-maker <davidokpah396@gmail.com>
2024-10-19 04:55:56 +0100

    modify file for stripe payment

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,10 +1,9 @@
 class CheckoutsController < ApplicationController
   before_action :authenticate_user!
-  
-  stripe_secret_key = Rails.application.credentials.dig(:stripe, :secret_key)
-  Stripe.api_key = stripe_secret_key
 
   def create
+    Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+
     course = Course.find(params[:course_id])
 
     session = Stripe::Checkout::Session.create(
@@ -13,13 +12,13 @@ class CheckoutsController < ApplicationController
         price: course.stripe_price_id,
         quantity: 1,
       }],
-      sucess_url: request.base_url + "/courses/#{course.id},",
-      cancel_url: request.base_url + "/courses/#{course.id}",
+      success_url: request.base_url + "/courses/#{course.id}/success",  # Corrected spelling of 'success_url'
+      cancel_url: request.base_url + "/courses/#{course.id}/cancel",    # Added '/cancel' to the URL for clarity
       automatic_tax: { enabled: true },
       customer_email: current_user.email,
       metadata: { course_id: course.id }
     )
 
-    redirect_to session_url, allowed_other_host: true
+    redirect_to session.url, allow_other_host: true  # Fixed typo here
   end
 end
