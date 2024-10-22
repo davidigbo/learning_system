4f4d06da5d9f80c9ef8a9c35b800e19380f18b5d
david30-maker <davidokpah396@gmail.com>
2024-10-22 12:04:50 +0100

    work on stripe payment

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,9 +1,6 @@
 class CheckoutsController < ApplicationController
   before_action :authenticate_user!
 
-  stripe_secret_key = Rails.application.credentials.dig(:stripe, :secret_key)
-  Stripe.api_key = stripe_secret_key
-
   def create
     course = Course.find(params[:course_id])
 
@@ -18,13 +15,13 @@ class CheckoutsController < ApplicationController
         price: course.stripe_price_id,
         quantity: 1,
       }],
-      success_url: request.base_url + "/courses/#{course.id}",  
-      cancel_url: request.base_url + "/courses/#{course.id}",  
-      automatic_tax: { enabled: true },
+      success_url: course_url(course),
+      cancel_url: course_url(course),
+      automatic_tax: {enabled: true},
       customer_email: current_user.email,
       metadata: { course_id: course.id }
     )
 
-    redirect_to session.url, allow_other_host: true 
+    redirect_to session.url, allow_other_host: true
   end
 end
