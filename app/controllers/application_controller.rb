c0adc55b12ce4d86ef3c3ee1ee3aca97e7a62341
david30-maker <davidokpah396@gmail.com>
2024-12-08 20:03:57 +0100

    add admin controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    include SessionsHelper
+    include ApplicationHelper
+    
+    def logged_in_user
+        unless logged_in?
+        store_location
+        flash[:danger] = 'Please log in.'
+        redirect_to login_url
+        end
+    end
+    
+    def admin_user
+        unless current_user.admin?
+        flash[:danger] = 'You are not an admin.'
+        redirect_to root_url
+        end
+    end
 end
